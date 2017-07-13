fef5a9eb26c746201c431024a3d10b26123f7402
Yang <yliuosu@gmail.com>
2017-07-13 11:37:28 -0500

    feat: Rearrange the updateWeights function to make it more readable.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -132,57 +132,6 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
 	
-	
-	const double sigma_xx = std_landmark[0]*std_landmark[0];
-	const double sigma_yy = std_landmark[1]*std_landmark[1];
-	const double k = 2 * M_PI * std_landmark[0] * std_landmark[1];
-	double dx = 0.0;
-	double dy = 0.0;
-	double sum_w = 0.0; // Sum of weights for future weights normalization
-	
-	for (int i = 0; i < num_particles; i++){
-		double weight_no_exp = 0.0;
-		const double sin_theta = sin(particles[i].theta);
-		const double cos_theta = cos(particles[i].theta);
-		for (int j = 0; j < observations.size(); j++){
-			// Observation measurement transformations
-			LandmarkObs observation;
-			observation.id = observations[j].id;
-			observation.x = particles[i].x + (observations[j].x * cos_theta) - (observations[j].y * sin_theta);
-			observation.y = particles[i].y + (observations[j].x * sin_theta) + (observations[j].y * cos_theta);
-			// Unefficient way for observation asossiation to landmarks. It can be improved.
-			bool in_range = false;
-			Map::single_landmark_s nearest_lm;
-            double nearest_dist = 10000000.0; // A big number
-            for (int k = 0; k < map_landmarks.landmark_list.size(); k++) {
-                Map::single_landmark_s cond_lm = map_landmarks.landmark_list[k];
-                double distance = dist(cond_lm.x_f, cond_lm.y_f, observation.x, observation.y);  // Calculate the Euclidean distance between two 2D points
-                if (distance < nearest_dist) {
-                    nearest_dist = distance;
-                    nearest_lm = cond_lm;
-                    if (distance < sensor_range){
-						in_range = true;
-					}
-                }
-            }
-            if (in_range){
-				dx = observation.x-nearest_lm.x_f;
-				dy = observation.y-nearest_lm.y_f;
-				weight_no_exp += dx * dx / sigma_xx + dy * dy / sigma_yy;
-			}
-			else {
-				weight_no_exp += 100; // approx = 0 after exp()
-			}
-		}
-		particles[i].weight = exp(-0.5*weight_no_exp); // calculate exp() after main computation in order to optimize the code
-		sum_w += particles[i].weight;
-	}
-	// Weights normalization to sum(weights)=1
-	for (int i = 0; i < num_particles; i++){
-		particles[i].weight /= sum_w * k;
-		weights[i] = particles[i].weight;
-	}
-	
 	// constants used later for calculating the new weights
  	const double stdx = std_landmark[0];
  	const double stdy = std_landmark[1];
